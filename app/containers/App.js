a8620b5e75ad3624d993f284a648e06463862ea8
FuYaoDe <dan82625@gmail.com>
2016-06-24 21:57:22 +0800

    add ant ui

--- app/containers/App.js
+++ app/containers/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Age from '../components/Age';
+import Option from './Option';
 import * as action from '../actions/SampleActions';
 import axios from 'axios';
+import { Popover } from 'antd';
 
 const styles = {
   container: {
@@ -12,6 +14,17 @@ const styles = {
     alignItems: 'center',
     backgroundColor: '#193441',
   },
+  setting: {
+    position: 'absolute',
+    right: 10,
+    top: 10,
+    fontSize: '2vh',
+    color: '#3E606F',
+  },
+  optionContainer: {
+    height: '200px',
+    width: '400px',
+  }
 };
 
 class App extends Component {
@@ -22,52 +35,76 @@ class App extends Component {
       time: new Date().getTime(),
       year: 0,
       day: 999999999,
+      isOptionOpen: false,
     };
   }
 
-  componentWillMount() {
-    const time = new Date(this.state.birthday).getTime();
-    this.setState({
-      time
-    });
+  // componentWillMount() {
+  //   const time = new Date(this.state.birthday).getTime();
+  //   this.setState({
+  //     time
+  //   });
+  //
+  //   const Animate = setInterval(() => {
+  //     const now = new Date().getTime();
+  //     let time = now - this.state.time;
+  //     time = time / 31556900000;
+  //     time = time.toFixed(9).toString().split('.');
+  //     if (this.state.year == time[0]) {
+  //       clearInterval(Animate);
+  //       this.genAge();
+  //       this.setState({
+  //         year: time[0],
+  //         day: time[1],
+  //       });
+  //     } else {
+  //       this.setState({
+  //         year: this.state.year + 1,
+  //         day: time[1],
+  //       });
+  //     }
+  //   }, 15);
+  // }
+  //
+  // genAge = () => {
+  //   setInterval(() => {
+  //     const now = new Date().getTime();
+  //     let time = now - this.state.time;
+  //     time = time / 31556900000;
+  //     time = time.toFixed(9).toString().split('.');
+  //     this.setState({
+  //       year: time[0],
+  //       day: time[1],
+  //     });
+  //   }, 0);
+  // }
 
-    const Animate = setInterval(() => {
-      const now = new Date().getTime();
-      let time = now - this.state.time;
-      time = time / 31556900000;
-      time = time.toFixed(9).toString().split('.');
-      if (this.state.year == time[0]) {
-        clearInterval(Animate);
-        this.genAge();
-        this.setState({
-          year: time[0],
-          day: time[1],
-        });
-      } else {
-        this.setState({
-          year: this.state.year + 1,
-          day: time[1],
-        });
-      }
-    }, 15);
+  openOption = () => {
+    this.setState({
+      isOptionOpen: !this.state.isOptionOpen,
+    });
   }
 
-  genAge = () => {
-    setInterval(() => {
-      const now = new Date().getTime();
-      let time = now - this.state.time;
-      time = time / 31556900000;
-      time = time.toFixed(9).toString().split('.');
-      this.setState({
-        year: time[0],
-        day: time[1],
-      });
-    }, 0);
+  renderOption = () => {
+    return (
+      <div style={styles.optionContainer}>
+        <Option />
+      </div>
+    );
   }
 
   render() {
     return (
       <div style={styles.container}>
+        <Popover content={this.renderOption()}
+          visible={this.state.isOptionOpen}
+          trigger="click"
+          placement="bottomRight" title="設定"
+        >
+          <div style={styles.setting} onClick={this.openOption}>
+            option
+          </div>
+        </Popover>
         <Age year={this.state.year} day={this.state.day} />
       </div>
     );
---
+++ app/containers/Option.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as action from '../actions/SampleActions';
+import { DatePicker, Tabs, Select, TimePicker, Button } from 'antd';
+const TabPane = Tabs.TabPane;
+const SelectOption = Select.Option;
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  setting: {
+    position: 'absolute',
+    right: 10,
+    top: 10,
+    fontSize: '2vh',
+    color: '#3E606F',
+  },
+  row: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  inputTitle: {
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: 10,
+  }
+};
+
+class Option extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="年齡" key="1">
+            <div style={styles.row}>
+              <div style={styles.inputTitle}>生日: </div>
+              <DatePicker format="yyyy/MM/dd" />
+            </div>
+            <div style={styles.row}>
+              <div style={styles.inputTitle}>顏色: </div>
+              <Select defaultValue="black" style={{ width: 160 }}>
+                <SelectOption value="black">black</SelectOption>
+                <SelectOption value="pink">pink</SelectOption>
+              </Select>
+            </div>
+            <div style={styles.row}>
+              <Button type="primary" >設定</Button>
+            </div>
+          </TabPane>
+          <TabPane tab="下班" key="2">
+            <div style={styles.row}>
+              <div style={styles.inputTitle}>時間: </div>
+              <TimePicker defaultValue={'06:30'} format="HH:mm" />
+            </div>
+            <div style={styles.row}>
+              <div style={styles.inputTitle}>顏色: </div>
+              <Select defaultValue="black" style={{ width: 160 }}>
+                <SelectOption value="black">black</SelectOption>
+                <SelectOption value="pink">pink</SelectOption>
+              </Select>
+            </div>
+            <div style={styles.row}>
+              <Button type="primary" >設定</Button>
+            </div>
+          </TabPane>
+          <TabPane tab="due day" key="3">
+            <div style={styles.row}>
+              <div style={styles.inputTitle}>日期: </div>
+              <DatePicker format="yyyy/MM/dd" />
+            </div>
+            <div style={styles.row}>
+              <div style={styles.inputTitle}>時間: </div>
+              <TimePicker defaultValue={'06:30'} format="HH:mm" />
+            </div>
+            <div style={styles.row}>
+              <div style={styles.inputTitle}>顏色: </div>
+              <Select defaultValue="black" style={{ width: 160 }}>
+                <SelectOption value="black">black</SelectOption>
+                <SelectOption value="pink">pink</SelectOption>
+              </Select>
+            </div>
+            <div style={styles.row}>
+              <Button type="primary" >設定</Button>
+            </div>
+          </TabPane>
+        </Tabs>
+      </div>
+    );
+  }
+}
+
+Option.propTypes = {
+  update: React.PropTypes.func,
+};
+
+function mapStateToProps(state) {
+  return {
+    info: state.info,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(action, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Option);

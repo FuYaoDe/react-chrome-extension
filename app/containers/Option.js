76719b2d96095d01772240f293d98bcc3495ab33
FuYaoDe <dan82625@gmail.com>
2016-06-26 04:12:18 +0800

    set config

--- app/containers/Option.js
+++ app/containers/Option.js
@@ -44,7 +44,24 @@ class Option extends Component {
     this.updateStorage('birthday', 'date', date.toString());
   }
   onBirthdayColorHandle = (value) => {
-    this.updateStorage('birthday', 'color', date.toString());
+    this.updateStorage('birthday', 'color', value);
+  }
+
+  onOffWorkHandle = (date) => {
+    this.updateStorage('offWork', 'time', date.toString());
+  }
+  onOffWorkColorHandle = (value) => {
+    this.updateStorage('offWork', 'color', value);
+  }
+
+  onDueDayDateHandle = (date) => {
+    this.updateStorage('dueDay', 'date', date.toString());
+  }
+  onDueDayTimeHandle = (date) => {
+    this.updateStorage('dueDay', 'time', date.toString());
+  }
+  onDueDayColorHandle = (value) => {
+    this.updateStorage('dueDay', 'color', value);
   }
 
   updateStorage = (type, key, value) => {
@@ -56,7 +73,7 @@ class Option extends Component {
         newData[type][key] = value;
         chrome.storage.sync.set({ config: newData }, () => {
           chrome.storage.sync.get('config', (data) => {
-            console.log('finish!!', data);
+            console.log('finish!!', JSON.stringify(data, null, 2));
           });
         });
       })
@@ -76,7 +93,8 @@ class Option extends Component {
             </div>
             <div style={styles.row}>
               <div style={styles.inputTitle}>顏色: </div>
-              <Select defaultValue="black" style={{ width: 160 }}
+              <Select
+                defaultValue="black" style={{ width: 160 }}
                 onChange={this.onBirthdayColorHandle}
               >
                 <SelectOption value="black">black</SelectOption>
@@ -90,11 +108,14 @@ class Option extends Component {
           <TabPane tab="下班" key="2">
             <div style={styles.row}>
               <div style={styles.inputTitle}>時間: </div>
-              <TimePicker defaultValue={'06:30'} format="HH:mm" />
+              <TimePicker defaultValue={'06:30'} format="HH:mm" onChange={this.onOffWorkHandle} />
             </div>
             <div style={styles.row}>
               <div style={styles.inputTitle}>顏色: </div>
-              <Select defaultValue="black" style={{ width: 160 }}>
+              <Select
+                defaultValue="black" style={{ width: 160 }}
+                onChange={this.onOffWorkColorHandle}
+              >
                 <SelectOption value="black">black</SelectOption>
                 <SelectOption value="pink">pink</SelectOption>
               </Select>
@@ -106,15 +127,21 @@ class Option extends Component {
           <TabPane tab="due day" key="3">
             <div style={styles.row}>
               <div style={styles.inputTitle}>日期: </div>
-              <DatePicker format="yyyy/MM/dd" />
+              <DatePicker format="yyyy/MM/dd" onChange={this.onDueDayDateHandle} />
             </div>
             <div style={styles.row}>
               <div style={styles.inputTitle}>時間: </div>
-              <TimePicker defaultValue={'06:30'} format="HH:mm" />
+              <TimePicker
+                defaultValue={'06:30'} format="HH:mm"
+                onChange={this.onDueDayTimeHandle}
+              />
             </div>
             <div style={styles.row}>
               <div style={styles.inputTitle}>顏色: </div>
-              <Select defaultValue="black" style={{ width: 160 }}>
+              <Select
+                defaultValue="black" style={{ width: 160 }}
+                onChange={this.onDueDayColorHandle}
+              >
                 <SelectOption value="black">black</SelectOption>
                 <SelectOption value="pink">pink</SelectOption>
               </Select>
